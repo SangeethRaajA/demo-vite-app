6cd72ff6266b1441a0a6997b8234d5ad54a0968e
Sangee <47982022+SangeethRaajA@users.noreply.github.com>
2024-01-04 15:39:07 +0530

    handle Event & state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+// import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup /></div>;
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // const handleSelectItem = (item: string) =>{
+  //   console.log(item);
+  // }
+
+  return (
+    <div>
+      {/* <ListGroup items={items} heading="Cities" onSelectItems={handleSelectItem}/> */}
+      {/* <Alert text="Hello World"/> */}
+      {/* <Alert> Hello <span> World </span> </Alert> */}
+      <Button color="secondary" onClick={() => console.log("CLick")}>My Button</Button>
+      {/* <Button color="react" onClick={() => console.log("CLick")}>My Button</Button> */}
+    </div>
+  );
 }
 
 export default App;
